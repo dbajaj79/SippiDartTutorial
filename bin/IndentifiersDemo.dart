d73a8e696a9c401c818c5b56ab5989bb58abe93e
deepakbconsultpr <deepakb@consultpr.net>
2019-11-22 12:40:01 +0530

    Indentifier in Dart

---
+++ bin/IndentifiersDemo.dart
@@ -0,0 +1,12 @@
+main()
+{
+ // var name deepak; //Wrong Space not allowed
+  var first_name;
+  //var 1one; //wrong
+  var $name;
+  var _deepak;
+  //var last-name; Wrong
+  var namedeepak="Deepak";
+  print(namedeepak);
+
+}
